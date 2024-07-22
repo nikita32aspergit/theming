9a63f3d83a4573ecd8afd2ded604f38cd159e32a
nikita32aspergit <nv966818@gmail.com>
2024-07-22 12:16:21 +0530

    initial commit

---
+++ web/themes/custom/mytheme/js/slider.js
@@ -0,0 +1,19 @@
+// const slider = ($) => {
+//   $(document).ready(function () {
+//     console.log("Document ready");
+//     $(".owl-carousel").owlCarousel({
+//       items: 3,
+//       loop: true,
+//       margin: 10,
+//       nav: true,
+//       dots: true,
+//       arrows: true,
+//       autoplay: true,
+//       autoplayTimeout: 5000,
+//       autoplayHoverPause: true,
+//     });
+//   });
+// };
+
+// slider(jQuery);
+
